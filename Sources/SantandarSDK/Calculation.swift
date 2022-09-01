f83c48d642cb29ef0701dd218a80c13305cfb3d3
Mohit Sharma <mohit.sharma@niit-tech.com>
2022-09-01 12:06:11 +0530

    initial

---
+++ Sources/SantandarSDK/Calculation.swift
@@ -0,0 +1,98 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mohit Sharma on 01/09/22.
+//
+
+import Foundation
+import UIKit
+import WebKit
+public class FrameworkClass
+{
+    public init()
+    {
+    }
+    var containerView = UIView()
+    public func Popup(view:UIViewController){
+        let anotherAlert = UIAlertController(title: "New One", message: "The Previous one is dismissed", preferredStyle: .alert)
+           let okAction = UIAlertAction(title: "OK", style: .default, handler: {action in
+                  let nextAlert = UIAlertController(title: "Second one", message: "The Previous one is dismissed", preferredStyle: .alert)
+               let okAction1 = UIAlertAction(title: "OK", style: .default, handler: {action in
+
+               })
+               nextAlert.addAction(okAction1)
+               view.present(nextAlert, animated: true, completion: nil)
+           })
+           anotherAlert.addAction(okAction)
+        view.present(anotherAlert, animated: true, completion: nil)
+    }
+    
+    public func fullScreen(view:UIViewController)  {
+       
+        containerView.frame = CGRect(x: 10, y: 10, width: view.view.frame.width-20, height: view.view.frame.height-20)
+        containerView.backgroundColor = UIColor.gray
+        let button = UIButton(frame: CGRect(x:  50, y: 50, width: Int(view.view.frame.width) - 100, height: 50))
+        button.backgroundColor = .blue
+        button.setTitle("dismiss", for: .normal)
+        button.addTarget(view, action: #selector(dismiss), for: .touchUpInside)
+        let  webView = WKWebView()
+       // webView.navigationDelegate = self
+        containerView = webView
+
+        let url = URL(string: "https://www.earthhero.org")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        containerView.addSubview(webView)
+        containerView.addSubview(button)
+        view.view.addSubview(containerView)
+
+    }
+    @objc func dismiss(){
+        containerView.removeFromSuperview()
+    }
+    public func addDouble( left:Double, right:Double ) -> Double
+    {
+        let sum = left + right
+        
+        #if DEBUG
+            print("\(left) + \(right) = \(sum)")
+        #endif
+        
+        return sum
+    }
+    
+    public func add( left:Int, right:Int ) -> Int
+    {
+        let sum = left + right
+        
+        #if DEBUG
+            print("\(left) + \(right) = \(sum)")
+        #endif
+        
+        return sum
+    }
+    
+    public func subtract( left:Int, right:Int ) -> Int
+    {
+        let remainder = left - right
+        
+        #if DEBUG
+            print("\(left) - \(right) = \(remainder)")
+        #endif
+        
+        return remainder
+    }
+    
+    public func multiply( left:Int, right:Int ) -> Int
+    {
+        let multiple = left * right
+        
+        #if DEBUG
+            print("\(left) * \(right) = \(multiple)")
+        #endif
+        
+        return multiple
+    }
+ 
+} // end public class FrameworkClass
---
+++ Tests/SantandarSDKTests/SantandarSDKTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SantandarSDK
+
+final class SantandarSDKTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SantandarSDK().text, "Hello, World!")
+    }
+}
