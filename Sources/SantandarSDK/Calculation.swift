048f8521d1a23421afcda8798fc7785b505e62c2
Mohit Sharma <mohit.sharma@niit-tech.com>
2022-09-04 13:44:29 +0530

    added loader

--- Sources/SantandarSDK/Calculation.swift
+++ Sources/SantandarSDK/Calculation.swift
@@ -11,7 +11,7 @@ import WebKit
 public class FrameworkClass
 {
    public var isLoaderEnable:Bool = false
-    var containerView = UIView()
+    
     public init()
     {
     }
@@ -30,33 +30,7 @@ public class FrameworkClass
         view.present(anotherAlert, animated: true, completion: nil)
     }
     
-    public func fullScreen(view:UIViewController, url:URL,containerView:UIView)  {
-   
-   containerView.frame = CGRect(x: 10, y: 40, width: view.view.frame.width-20, height: view.view.frame.height-80)
-   containerView.backgroundColor = UIColor.gray
-    containerView.layer.cornerRadius = 8
-    // MARK: button
-    let button = UIButton(frame: CGRect(x: 40, y: view.view.frame.height - 160, width: view.view.frame.width - 80, height: 50))
-    button.backgroundColor = .blue
-    button.layer.cornerRadius = 8
-    button.setTitle("Submit button", for: .normal)
-    
-        button.addTarget(self, action: #selector(FrameworkClass().buttonAction), for: .touchUpInside)
- 
-    let webV:UIWebView = UIWebView(frame: CGRect(x: 10, y: 10, width: containerView.frame.width - 20, height: view.view.frame.height-200))
-    webV.backgroundColor = .clear
-    webV.layer.cornerRadius = 8
-    webV.loadRequest(URLRequest(url: url))
-    
-    containerView.addSubview(webV)
-    containerView.addSubview(button)
-    view.view.addSubview(containerView)
 
-  }
-    
-   @objc func buttonAction(){
-       containerView.removeFromSuperview()
-   }
     public func addDouble( left:Double, right:Double ) -> Double
     {
         let sum = left + right
--- Sources/SantandarSDK/View/View.swift
+++ Sources/SantandarSDK/View/View.swift
@@ -8,9 +8,33 @@
 import Foundation
 import UIKit
 public class webView{
-
- 
+    var containerView = UIView()
+    public func fullScreen(view:UIViewController, url:URL,containerView:UIView)  {
+   
+   containerView.frame = CGRect(x: 10, y: 40, width: view.view.frame.width-20, height: view.view.frame.height-80)
+   containerView.backgroundColor = UIColor.gray
+    containerView.layer.cornerRadius = 8
+    // MARK: button
+    let button = UIButton(frame: CGRect(x: 40, y: view.view.frame.height - 160, width: view.view.frame.width - 80, height: 50))
+    button.backgroundColor = .blue
+    button.layer.cornerRadius = 8
+    button.setTitle("Submit button", for: .normal)
     
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
  
+    let webV:UIWebView = UIWebView(frame: CGRect(x: 10, y: 10, width: containerView.frame.width - 20, height: view.view.frame.height-200))
+    webV.backgroundColor = .clear
+    webV.layer.cornerRadius = 8
+    webV.loadRequest(URLRequest(url: url))
+    
+    containerView.addSubview(webV)
+    containerView.addSubview(button)
+    view.view.addSubview(containerView)
+
+  }
+    
+   @objc func buttonAction(){
+       containerView.removeFromSuperview()
+   }
     
 }
