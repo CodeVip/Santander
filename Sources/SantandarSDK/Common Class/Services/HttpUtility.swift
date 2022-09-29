51ca4687335edf1e8b2e6a6063a12dce8961b56c
Mohit Sharma <mohit.sharma@niit-tech.com>
2022-09-29 10:33:24 +0530

    APi changes

--- Sources/SantandarSDK/Common Class/Services/HttpUtility.swift
+++ Sources/SantandarSDK/Common Class/Services/HttpUtility.swift
@@ -75,7 +75,7 @@ class HttpUtility: NSObject {
                 return
             }
             do{
-                let json = try! JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
+                let json = try! JSONSerialization.jsonObject(with: data, options: .allowFragments)
                 print(json)
                 let result = try JSONDecoder().decode(type, from: data)
                 Log.d("Parsing done sucessfully:\(result)")
