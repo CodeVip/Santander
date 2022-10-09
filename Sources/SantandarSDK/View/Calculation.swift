1913e2cd4ae99d2cb9003d0c5a8e87a9c947bad9
Mohit Sharma <mohit.sharma@niit-tech.com>
2022-10-09 21:19:25 +0530

    Add the NFC card Reader

--- Sources/SantandarSDK/View/Calculation.swift
+++ Sources/SantandarSDK/View/Calculation.swift
@@ -8,13 +8,17 @@
 import Foundation
 import UIKit
 import WebKit
+import CoreNFC
 
-public class FrameworkClass
+public class FrameworkClass:NSObject
 {
     var containerView = UIView()
     public var isLoaderEnable:Bool = false
     public var isLoggingEnabled:Bool = false
     public var localization:String = ""
+    var session: NFCNDEFReaderSession?
+      // var productStore = ProductStore.shared
+   var view1:UIViewController?
     
     lazy var viewModel:SantanderViewModel! = {
         let service = HttpUtility()
@@ -37,11 +41,22 @@ public class FrameworkClass
     
     // MARK: Api call
     
-    public func readCard(){
-        let viewCont = ViewController()
-        viewCont.beginReadSeesion()
+    public func readCard(view:UIViewController){
+//        let viewCont = ViewController()
+//        viewCont.beginReadSeesion()
+        
+        guard session == nil else {
+                   return
+               }
+               session = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: true)
+               session?.alertMessage = "Hold your iPhone near the item to learn more about it."
+               session?.begin()
+        view1 = view
         
     }
+    
+    
+    
  public func APICll(baseUrl:String,body:NSMutableDictionary?,view:UIViewController,completion:@escaping (Bool,Int)->()){
     if isLoaderEnable {
         DPLoader.show(InView: view.view.self, "Loading")
@@ -129,4 +144,63 @@ public class FrameworkClass
 //            }
 //        }
 //    }
+}
+
+extension FrameworkClass:NFCNDEFReaderSessionDelegate{
+    /// - Tag: processingTagData
+    public func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        
+        guard
+            let ndefMessage = messages.first,
+            let record = ndefMessage.records.first,
+            record.typeNameFormat == .absoluteURI || record.typeNameFormat == .nfcWellKnown,
+            let payloadText = String(data: record.payload, encoding: .utf8),
+            let sku = payloadText.split(separator: "/").last else {
+            return
+        }
+        
+        
+        self.session = nil
+        
+//        guard let product = productStore.product(withID: String(sku)) else {
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 3) { [weak self] in
+//                let alertController = UIAlertController(title: "Info", message: "SKU Not found in catalog",preferredStyle: .alert)
+//                alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//                // view1!.view.present(alertController, animated: true, completion: nil)
+//            }
+//            return
+//        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) { [weak self] in
+            //  self?.presentProductViewController(product: product)
+        }
+    }
+    
+    
+    
+    
+    /// - Tag: endScanning
+    public func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        // Check the invalidation reason from the returned error.
+        if let readerError = error as? NFCReaderError {
+            // Show an alert when the invalidation reason is not because of a success read
+            // during a single tag read mode, or user canceled a multi-tag read mode session
+            // from the UI or programmatically using the invalidate method call.
+            if (readerError.code != .readerSessionInvalidationErrorFirstNDEFTagRead)
+                && (readerError.code != .readerSessionInvalidationErrorUserCanceled) {
+                let alertController = UIAlertController(
+                    title: "Session Invalidated",
+                    message: error.localizedDescription,
+                    preferredStyle: .alert
+                )
+                alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                DispatchQueue.main.async {
+                    //  self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+        
+        // A new session instance is required to read new tags.
+        self.session = nil
+    }
 }
